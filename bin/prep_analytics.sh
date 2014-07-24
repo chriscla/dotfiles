3db5d796072dc41715e0f0c9d71c585528d05a94
Chris Clark <chriscla@gmail.com>
2014-07-23 20:00:35 -0400

    Added basic project script

---
+++ bin/prep_analytics.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# # prep: prep analytics directory structure #
+# usage: prep DIRNAME # 
+
+if [ "$#" == "0" ]; then 
+	echo "ERROR: Please specify a directory name"
+	echo 
+	echo "USAGE: prep DIRNAME" 
+fi
+
+DIR=$1
+if [ ! -d "${DIR}" ]; then
+	mkdir -p ${DIR}/R \
+	${DIR}/data \
+	${DIR}/docs \
+	${DIR}/output \
+	${DIR}/python \
+	${DIR}/tmp
+	> ${DIR}/readme.md
+	ls -lR ${DIR}
+else
+	echo "Directory "${DIR}" already exists"
+fi
